712bfa2a9a9afa3be58e21e844fc1ae6826fcbd2
Yoan Petrov <yoan.petrov97@gmail.com>
2019-05-13 21:25:01 +0300

    Added empty tests

---
+++ src/test/java/bz/sty/internship/school/SchoolTest.java
@@ -0,0 +1,46 @@
+package bz.sty.internship.school;
+
+import org.testng.annotations.Test;
+
+public class SchoolTest {
+
+    @Test
+    public void testAddCourse() {
+    }
+
+    @Test
+    public void testAddStudent() {
+    }
+
+    @Test
+    public void testAddTeacher() {
+    }
+
+    @Test
+    public void testAddTeacherToCourse() {
+    }
+
+    @Test
+    public void testAddStudentToCourse() {
+    }
+
+    @Test
+    public void testAddGradeForStudentInCourse() {
+    }
+
+    @Test
+    public void testShowAllStudents() {
+    }
+
+    @Test
+    public void testShowAllCoursesAndTheirTeachersAndStudents() {
+    }
+
+    @Test
+    public void testShowAverageGradeForAllStudentsInCourse() {
+    }
+
+    @Test
+    public void testShowAverageGradeForAllCourses() {
+    }
+}
